1ee4e548da8b87a882e1409b6a19d19f697c59b1
enzocardeal <enzocardeal@usp.br>
2022-09-02 15:07:15 -0300

    PR fix

--- App.tsx
+++ App.tsx
@@ -21,10 +21,6 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 const Section: React.FC<
--- READEME.md
+++
@@ -1,4 +0,0 @@
-Onboarding project
-Small project to learn the company development cycle
-Environment and tools: none yet
-Steps to run and debug: none yet
