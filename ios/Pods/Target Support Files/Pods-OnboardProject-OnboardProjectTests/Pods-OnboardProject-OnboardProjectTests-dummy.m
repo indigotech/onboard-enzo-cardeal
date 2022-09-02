52222950d7de9644d7543c8da64f4ee6fcfd29a8
enzocardeal <enzocardeal@usp.br>
2022-09-02 11:34:51 -0300

    initial setup for iOS and Android app

---
+++ ios/Pods/Target Support Files/Pods-OnboardProject-OnboardProjectTests/Pods-OnboardProject-OnboardProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OnboardProject_OnboardProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_OnboardProject_OnboardProjectTests
+@end
