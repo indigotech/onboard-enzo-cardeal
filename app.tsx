354152c7d8abdafada523d489424c705adc861b5
enzocardeal <enzocardeal@usp.br>
2022-09-06 12:04:24 -0300

    code refactor

--- app.tsx
+++ app.tsx
@@ -69,22 +69,23 @@ const App = () => {
     const isEmailValid = emailPattern.test(email);
     const isPasswordValid = passwordPattern.test(password);
 
+    let alertTitle = '';
+    let alertDescription = '';
     if (!isEmailValid && !isPasswordValid) {
-      Alert.alert(
-        'E-mail e sennha inválidos.',
-        "Por favor, insira um e-mail no formato '####@####.com', e uma senha com pelo menos 7 caracteres sendo pelo menos uma letra e um número.",
-        [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
-      );
+      alertTitle = 'E-mail e sennha inválidos.';
+      alertDescription =
+        "Por favor, insira um e-mail no formato '####@####.com', e uma senha com pelo menos 7 caracteres sendo pelo menos uma letra e um número.";
     } else if (!isEmailValid && isPasswordValid) {
-      Alert.alert('E-mail inválido.', 'Por favor, innsira um e-mail no formato: ####@####.com.', [
-        { text: 'OK', onPress: () => console.log('OK Pressed') },
-      ]);
+      alertTitle = 'E-mail inválido.';
+      alertDescription = 'Por favor, innsira um e-mail no formato: ####@####.com.';
     } else if (!isPasswordValid) {
-      Alert.alert(
-        'Senha inválida.',
-        'Por favor, insira uma senha com pelo menos 7 caracteres, contendo pelo menos uma letra e um número',
-        [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
-      );
+      alertTitle = 'Senha inválida.';
+      alertDescription =
+        'Por favor, insira uma senha com pelo menos 7 caracteres, contendo pelo menos uma letra e um número';
+    }
+
+    if (!isEmailValid || !isPasswordValid) {
+      Alert.alert(alertTitle, alertDescription, [{ text: 'OK', onPress: () => console.log('OK Pressed') }]);
     }
   };
 
