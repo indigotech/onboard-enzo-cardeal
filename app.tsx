d360f5335c209313a026cf485ecd0108336e5d33
enzocardeal <enzocardeal@usp.br>
2022-09-08 18:02:19 -0300

    removed log

--- app.tsx
+++ app.tsx
@@ -131,7 +131,6 @@ const App = (props: NavigationComponentProps) => {
       Alert.alert(alertTitle, alertDescription, [{ text: 'OK', onPress: () => console.log('OK Pressed') }]);
     } else {
       await login(loginData);
-      console.log(props);
       Navigation.push(props.componentId, {
         component: {
           name: 'BlankScreen',
