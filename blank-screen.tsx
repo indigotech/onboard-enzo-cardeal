0d881c5cf765f8de31adc627d0aa79f9b62634c9
enzocardeal <enzocardeal@usp.br>
2022-09-09 09:35:05 -0300

    mock file to populate list page + blank screen updated to be the list page

--- blank-screen.tsx
+++ blank-screen.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StatusBar, Text, useColorScheme, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { userList } from './mock';
 
 const BlankScreen = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  const listRender = userList.map((item) => {
+    return (
+      <View
+        style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: 0.5,
+        }}
+        key={item.id}
+      >
+        <Text>Usuário: {item.user}</Text>
+        <Text>E-mail: {item.email}</Text>
+      </View>
+    );
+  });
+
   return (
-    <View>
-      <Text>Hello from Taqtile 👋</Text>
-    </View>
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior='automatic' style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}
+        >
+          {listRender}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
