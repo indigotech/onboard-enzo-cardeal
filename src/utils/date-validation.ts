cfbe390c2163b7dbf16b47db7be979aa44bc9649
enzocardeal <enzocardeal@usp.br>
2022-09-15 09:05:16 -0300

    removed unnecessary date.parse

--- src/utils/date-validation.ts
+++ src/utils/date-validation.ts
@@ -1,7 +1,7 @@
 export const validateDate = (date: string) => {
   const dateList = date.split('/');
   date = dateList[2] + '-' + dateList[1] + '-' + dateList[0];
-  const pickedDate = new Date(Date.parse(date));
+  const pickedDate = new Date(date);
   const todaysDate = new Date();
   todaysDate.setHours(0, 0, 0, 0);
 
