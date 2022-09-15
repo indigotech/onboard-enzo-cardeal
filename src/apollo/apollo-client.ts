fcf5633fe000100935737b94dc7d24750d661ca0
enzocardeal <enzocardeal@usp.br>
2022-09-15 09:35:19 -0300

    removed unnecessary import

--- src/apollo/apollo-client.ts
+++ src/apollo/apollo-client.ts
@@ -1,14 +1,12 @@
 import { createHttpLink, ApolloClient, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { getAuthenticationToken } from '../utils/async-token-storage';
-import { offsetLimitPagination } from "@apollo/client/utilities";
 
 const httpLink = createHttpLink({
   uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
 });
 
 const authLink = setContext(async (_, { headers }) => {
-
   const token = await getAuthenticationToken();
 
   return {
