12ec7130de78ab0c5ca9f0e3c92da253b8986843
enzocardeal <enzocardeal@usp.br>
2022-09-19 10:32:28 -0300

    pr suggestion

--- src/user-details/user-details.tsx
+++ src/user-details/user-details.tsx
@@ -11,7 +11,6 @@ import {
   ActivityIndicator,
 } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { userDetails } from '../../resources/mock/user-details-mock';
 import { ErrorResponse, UserQueryDataResponse } from '../apollo/apollo-interfaces';
 import { userQuery } from '../apollo/queries';
 import { UserDetailProps } from './user-details-props';
@@ -21,22 +20,10 @@ const UserDetails = (props: UserDetailProps) => {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [birthDate, setBirthDate] = useState('');
-  const [email, setEmail] = useState('');
-  const [role, setRole] = useState('');
 
   const listUsersData = {
     variables: {
       ID: props.userId,
-    },
-    onCompleted: (response: UserQueryDataResponse) => {
-      setName(response.user.name);
-      setPhone(response.user.phone);
-      setBirthDate(response.user.birthDate);
-      setEmail(response.user.email);
-      setRole(response.user.role);
     },
     onError: (response: ErrorResponse) => {
       const errorMessage = response.message;
@@ -44,7 +31,9 @@ const UserDetails = (props: UserDetailProps) => {
     },
   };
 
-  const { loading } = useQuery(userQuery, listUsersData);
+  const { data, loading } = useQuery(userQuery, listUsersData);
+  const user = data?.user;
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -56,11 +45,11 @@ const UserDetails = (props: UserDetailProps) => {
               backgroundColor: isDarkMode ? Colors.black : Colors.white,
             }}
           >
-            <Text>Nome: {name}</Text>
-            <Text>Telefone: {phone}</Text>
-            <Text>Aniversário: {birthDate}</Text>
-            <Text>E-mail: {email}</Text>
-            <Text>Função: {role}</Text>
+            <Text>Nome: {user.name}</Text>
+            <Text>Telefone: {user.phone}</Text>
+            <Text>Aniversário: {user.birthDate}</Text>
+            <Text>E-mail: {user.email}</Text>
+            <Text>Função: {user.role}</Text>
           </View>
         )}
       </ScrollView>
